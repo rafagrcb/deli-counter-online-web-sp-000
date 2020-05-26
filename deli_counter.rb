74ec7d247bb39ef3fb0e88af35a202be7b483d7c
Rafael Garcia <rafagrcb@gmail.com>
2020-05-26 21:26:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,14 @@
 katz_deli = []
 
 def line(array)
-  if array[0] == nil
+  line_array = []
+  if x.length == 0
     puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
   end
 end
 
